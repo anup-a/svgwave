b50419c9c4c2ebf7ffac8386b21d85b19ff454d1
Yen Colon <yendejesusc41@gmail.com>
2020-10-17 09:40:03 -0400

    fix: misspelled typos

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "preact/hooks"
 
 const DARK = 'dark'
-const LIGHT =  'ligth'
+const LIGHT =  'light'
 
 export const useDarkModeState = () => {
     const [isDark, setIsDark] = useState(() => {
         const mode = localStorage.getItem('themeMode', LIGHT)
-        return mode === 'dark'
+        return mode === DARK
     });
 
     useEffect(() => {
